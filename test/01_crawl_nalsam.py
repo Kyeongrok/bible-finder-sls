d20c7e980966160170cb62f259038c6e721e4068
mattheu <mattheu@plutusds.com>
2019-05-28 18:08:15 +0900

    add cors true

---
+++ test/01_crawl_nalsam.py
@@ -0,0 +1,41 @@
+from libs.crawler import crawl
+from bs4 import BeautifulSoup
+import libs.bibleFinder as finder
+
+url = "http://www.365qt.com/TodaysQT.asp?QTID=7598"
+pageString = crawl(url)
+
+def parse(pageString):
+    result = {}
+    bsObj = BeautifulSoup(pageString, "html.parser")
+
+    qtDayText2 = bsObj.find("div", {"id":"qtDay"})
+    result['qtDayText2'] = qtDayText2.text
+
+    box2Content = bsObj.find("div", {"class":"box2Content"})
+    result['box2Content'] = box2Content.text
+
+    try:
+        script = bsObj.find("div", {"class":"script"})
+        result['srcipt'] = script.text
+    except Exception as e:
+        print(e)
+
+
+    content = bsObj.find("div", {"id":"content"})
+    ps = content.findAll("p")
+    result['ps4']=ps[4].text
+
+    bx2 = bsObj.find("div", {"class":"bx2"})
+    result['bx2']=bx2.text
+
+    return result
+
+result = parse(pageString)
+print(result['qtDayText2'])
+print(result['ps4'])
+print(result['bx2'])
+
+res = finder.findBetween("민",6,13,21)
+for re in res:
+    print(re['index'], re['text'])
